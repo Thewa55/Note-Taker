888fb0b0ead6a9c616262d4ff25b9f6eee154592
Kenny Lam <kinwai.lam730@gmail.com>
2020-02-23 23:27:45 -0500

    Reverted changes on index.js, didnt see declared variables on top

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,11 @@
 const noteData = require("../db/db.json")
 
 module.exports =function(app){
-  app.get("/api/notes", function(){
-    return res.json(noteData)
+  app.get("/api/notes", (req, res) => {
+    res.json(noteData)
   });
 
-  app.post("/api/notes", function(req, res){
+  app.post("/api/notes", (req, res) => {
     const Note = req.body
     noteData.push(Note)
   })
