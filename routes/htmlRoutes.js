02a7f30d0c4c5424514a1a7b5d1c0bcdd8cb3d99
Kenny Lam <kinwai.lam730@gmail.com>
2020-02-23 22:37:52 -0500

    Fixed an error on htmlRoute

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ var path = require("path")
 
 module.exports = function(app){
 
-  app.get("/api/notes", (req, res) =>
+  app.get("/notes", (req, res) =>
     res.sendFile(path.join(__dirname,"../public/notes.html"))
   );
 
