28c75eacdc2ff6f9174aad026b6c06796daf6db3
Kenny Lam <kinwai.lam730@gmail.com>
2020-02-24 23:09:31 -0500

    Added a function in apiRoutes so it will generate a unique random number

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,16 +6,29 @@ module.exports =function(app){
     res.json(noteData)
   });
 
-  //this takes the variable "newNote" in index.js which was put through an ajax call. In the ajax call it generates a number 1 to 1,000,000 and saves it as an id number to the variable
-  //It also takes the body of newNote and pushes it into noteData array and returns the new array for rendering.
+  //this takes the variable "newNote" in index.js which was put through an ajax call. 
+  //it calls a function call randomID to randomly generate an ID for the object
+  //after getting the id, it takes the incoming object with a declared id and pushes it into noteData array and returns the new array for rendering.
   app.post("/api/notes", (req, res) => {
     const note = req.body
-    note.id = Math.floor(Math.random()*1000000)+1
+    var randNum = randomID()
+    note.id = randNum
     noteData.push(note)
     res.json(noteData)
   })
 
-
+  //this function will roll a number 1 to 1,000,000 which will check against itself to make sure that the number doesn't already exist.
+  //if the number does exist, it will regenerate another number. This sets the program to hold 1 million notes (which I think is enough)
+  function randomID(){
+    randNum = Math.floor(Math.random()*1000000)+1
+    console.log(randNum)
+    for(let i = 0; i < noteData.length; i++){
+      if(noteData[i].id == randNum){
+        randomID()
+      }
+    }
+    return randNum
+  }
   //this takes the id associated with the list item and put it through this ajax call. This takes the incoming request and set it as a parameter of the variable note.
   //The for-loop loops through the noteData array and the if statement checks to see if the chosen id is equal to the object id. 
   //Once it finds a match it splices the position of where it matches in noteData and returns the new array for rendering.
